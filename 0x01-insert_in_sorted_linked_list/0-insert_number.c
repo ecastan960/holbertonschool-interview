a800d6fb631539e35534f63a93256346aa1e7f54
ecastan960 <ecastan1@eafit.edu.co>
2022-04-28 21:49:07 -0500

    [00] upload files

---
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,47 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * insert_node - Function to insert a node in a SL list.
+ * @head: A double pointer that contains the head.
+ * @number: number to add in the list.
+ *
+ * Return: the new head
+ */
+
+listint_t *insert_node(listint_t **head, int number)
+{
+    listint_t *new = NULL,
+    *temp = NULL;
+
+    new = malloc(sizeof(listint_t));
+    if (new == NULL || head == NULL)
+        return NULL;
+    new->n = number;
+    new->next = NULL;
+
+    if (*head == NULL)
+    {
+        new->n = number;
+        *head = new;
+        return (new);
+    }
+    temp = *head;
+    if (number <= temp->n)
+    {
+        new->n = number;
+        new->next = temp;
+        *head = new;
+        return (new);
+    }
+    while (temp->next)
+    {
+        if (temp->next->n >= number)
+            break;
+        temp = temp->next;
+    }
+    new->n = number;
+    new->next = temp->next;
+    temp->next = new;
+    return (new);
+}
