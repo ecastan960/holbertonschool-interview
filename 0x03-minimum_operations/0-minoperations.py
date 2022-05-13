7a59fe9b7e441176bfa80bb6cc521bf8ef1cf17e
ecastan960 <ecastan1@eafit.edu.co>
2022-05-12 23:19:22 -0500

    [00] upload files

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Method File minOperations"""
+import math
+
+def minOperations(n):
+    """Method that calculates the fewest number of
+    operation needed to result in exactly n H characters
+    in the file
+
+    Args:
+        n (number): input number
+    """    
+    ans = 0
+    if n > 1:
+        for i in range(2, int(math.sqrt(n) + 1)):
+            while n % i == 0:
+                ans = ans + i
+                n = n // i
+    if n > 1:
+        return ans + n
+    else:
+        return ans
--- 0x03-minimum_operations/README.md
+++ 0x03-minimum_operations/README.md
@@ -1,1 +1,1 @@
-
+# 0x03. Minimum Operations
