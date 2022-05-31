a935d9cd82fc857c684c9fd8b135c79661a6baec
ecastan960 <ecastan1@eafit.edu.co>
2022-05-30 22:38:14 -0500

    [00] Update files

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""0. Lockboxes
+    """
+
+
+def canUnlockAll(boxes):
+    """method that determines if all the boxes can be opened
+
+    Args:
+    boxes (list): list of locked boxes
+    """
+    keys = set()
+    for newKey, box in enumerate(boxes):
+        newKey = newKey + 1
+        for key in box:
+            if key < len(boxes):
+                keys.add(key)
+                keys.update(boxes[key])
+            if len(keys) == len(boxes) or len(keys) == len(boxes) - 1:
+                return True
+        if newKey not in keys:
+            return False
