9b5b8f8a9045b75648656c383483d49de7fa4aa1
ecastan960 <ecastan1@eafit.edu.co>
2022-08-21 09:41:44 -0500

    [00] test 1

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,11 +1,13 @@
 #!/usr/bin/node
 const request = require('request');
 request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, function (error, response, body) {
-  characters = JSON.parse(body);
+  if (error) console.log(error);
+  let characters = JSON.parse(body);
   characters = characters.characters;
   for (const character of characters) {
     request(`${character}`, function (error, response, body) {
-      let name = JSON.parse(body);
+      if (error) console.log(error);
+      const name = JSON.parse(body);
       console.log(name.name);
     });
   }
--- 0x0F-starwars_api/README.md
+++ 0x0F-starwars_api/README.md
@@ -1,1 +1,1 @@
-
+# 0x0F. Star Wars API
