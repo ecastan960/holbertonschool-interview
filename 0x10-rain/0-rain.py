9e9be6cc42821d62b404317a8ed99e17e68ce8ba
ecastan960 <ecastan1@eafit.edu.co>
2022-08-21 13:15:25 -0500

    [04] test 0

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -4,23 +4,23 @@
 
 
 def rain(walls):
-  """_summary_
+    """_summary_
 
-  Args:
-      walls (_type_): _description_
+    Args:
+        walls (_type_): _description_
 
-  Returns:
-      _type_: _description_
-  """
-  total = 0
-  size = len(walls)
+    Returns:
+        _type_: _description_
+    """
+    total = 0
+    size = len(walls)
 
-  for i in range(1, size - 1):
-    left = walls[i]
-    for j in range(i):
-      left = max(left, walls[j])
-    right = walls[i]
-    for j in range(i + 1, size):
-      right = max(right, walls[j])
-    total = total + (min(left, right) - walls[i])
-  return total
+    for i in range(1, size - 1):
+        left = walls[i]
+        for j in range(i):
+            left = max(left, walls[j])
+        right = walls[i]
+        for j in range(i + 1, size):
+            right = max(right, walls[j])
+        total = total + (min(left, right) - walls[i])
+    return total
