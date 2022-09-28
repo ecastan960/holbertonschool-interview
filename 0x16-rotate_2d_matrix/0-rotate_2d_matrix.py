8647a537611b08c1eafe446069e7b9bda48ce3bc
ecastan960 <ecastan1@eafit.edu.co>
2022-09-27 22:30:59 -0500

    [00] task 0

---
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""_summary_
+"""
+
+def rotate_2d_matrix(matrix):
+  """_summary_
+
+  Args:
+      matrix (_type_): _description_
+  """
+  matrix.reverse()
+  temp_matrix = matrix.copy()
+
+  for i in range(len(matrix)):
+    matrix[i] = [row[i] for row in temp_matrix]
