791768759450e390828733663e494f51399644e1
ecastan960 <ecastan1@eafit.edu.co>
2022-10-06 22:28:37 -0500

    [01] task 0

--- 0x11-heap_sort/sort.h
+++ 0x11-heap_sort/sort.h
@@ -1,6 +1,8 @@
 #ifndef _SORT_H_
 #define _SORT_H_
 
+#include <stddef.h>
+
 void maxHeap(int *array, int size, int i, int original);
 void print_array(const int *array, size_t size);
 void heap_sort(int *array, size_t size);
