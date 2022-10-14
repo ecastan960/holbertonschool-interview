8ba083fc62b4923d34272d5396eb43d95b59b6a7
ecastan960 <ecastan1@eafit.edu.co>
2022-10-13 23:34:29 -0500

    [02] Task 0

--- 0x0E-linear_skip/0-linear_skip.c
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -1,19 +1,83 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "search.h"
-
-
 /**
- * linear_skip - searches for a value in a sorted skip list
- *
- * @list: ptr to head of list
- * @value: value of element in list
- *
- * Return: pointer to first node where value is, or NULL
+ * print - print
+ * @f_idx: first_idx
+ * @s_idx: second_idx
+ * @end: know if the last one in express line
  */
-skiplist_t *linear_skip(skiplist_t *list, int value)
+void print(size_t f_idx, size_t s_idx, skiplist_t *end)
 {
-	if (!list || !value)
+	if (end != NULL)
+	{
+		while (end->next != NULL)
+			end = end->next;
+		s_idx = end->index;
+	}
+	printf("Value found between indexes [%lu] and [%lu]\n", f_idx, s_idx);
+}
+/**
+ * print_check - print
+ * @idx: index
+ * @value: value
+ */
+void print_check(size_t idx, int value)
+{
+	printf("Value checked at index [%lu] = [%i]\n", idx, value);
+}
+/**
+ * find - check next by next
+ * @head: express line node
+ * @value: value to compare
+ *
+ * Return: value or null
+ */
+skiplist_t *find(skiplist_t *head, int value)
+{
+	skiplist_t *current = head;
+
+	if (head == NULL)
+		printf("n head nullll");
+	for (; current; current = current->next)
+	{
+		print_check(current->index, current->n);
+		if (current->n == value)
+			return (current);
+	}
+	return (NULL);
+}
+/**
+ * linear_skip - found a value in skip list
+ * @head: express line node
+ * @value: value to compare
+ *
+ * Return: value or null
+ */
+skiplist_t *linear_skip(skiplist_t *head, int value)
+{
+	skiplist_t *current  = head, *next = NULL;
+
+	if (head == NULL)
 		return (NULL);
-	return (list);
+
+	while (current != NULL)
+	{
+		next = current->express;
+		if (next != NULL)
+		{
+			print_check(next->index, next->n);
+			if (next->n >= value)
+			{
+				print(current->index, next->index, NULL);
+				return (find(current, value));
+			}
+
+			if (next->express == NULL)
+			{
+				print(next->index, 0, next);
+				return (find(next, value));
+			}
+		}
+		current = current->express;
+	}
+	return (NULL);
 }
