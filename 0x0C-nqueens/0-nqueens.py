07bf9074fd6398325ebc2f8ab99172088f4c4036
ecastan960 <ecastan1@eafit.edu.co>
2022-10-17 14:58:04 -0500

    [02] Task 0

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-"""_summary_
-
-Returns:
-    _type_: _description_
-"""
+""" module docs """
 from sys import argv, exit
 
 if __name__ == "__main__":
@@ -24,41 +20,27 @@ if __name__ == "__main__":
         print("N must be at least 4")
         exit(1)
 
-    answer = []
+    solution = []
 
-    def nqueens(row, queens, answer):
-        """_summary_
-
-        Args:
-            row (_type_): _description_
-            queens (_type_): _description_
-            answer (_type_): _description_
-        """
+    def nqueens(row, queens, solution):
+        """ method docs """
         if (row == queens):
-            print(answer)
+            print(solution)
         else:
             for col in range(queens):
                 pos = [row, col]
-                if valid(answer, pos):
-                    answer.append(pos)
-                    nqueens(row + 1, queens, answer)
-                    answer.remove(pos)
+                if validposition(solution, pos):
+                    solution.append(pos)
+                    nqueens(row + 1, queens, solution)
+                    solution.remove(pos)
 
-    def valid(answer, position):
-        """_summary_
-
-        Args:
-            answer (_type_): _description_
-            position (_type_): _description_
-
-        Returns:
-            _type_: _description_
-        """
-        for queen in answer:
+    def validposition(solution, position):
+        """ method docs """
+        for queen in solution:
             if queen[1] == position[1] or \
                 (queen[0] - queen[1]) == (position[0] - position[1]) or \
                     (queen[0] + queen[1]) == (position[0] + position[1]):
                 return False
         return True
 
-    nqueens(0, queens, answer)
+    nqueens(0, queens, solution)
