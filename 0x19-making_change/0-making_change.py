23e1f3fcd20fbb22115030ebf3c7bde6c345b145
ecastan960 <ecastan1@eafit.edu.co>
2022-10-27 20:21:04 -0500

    [00] Task 0

---
+++ 0x19-making_change/0-making_change.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""_summary_
+"""
+import math
+
+
+def makeChange(coins, total):
+    """_summary_
+
+    Args:
+        coins (_type_): _description_
+        total (_type_): _description_
+    """
+    numberCoins = 0
+    remainingTotal = total
+    check = 0
+
+    for val in coins:
+        if (total / val).is_integer():
+            check = 0
+            break
+        check = 1
+
+    if check == 1:
+        return -1
+
+    coins.sort(reverse=True)
+    for val in coins:
+        numberCoins += math.floor(remainingTotal / val)
+        remainingTotal -= math.floor(remainingTotal / val) * val
+        if remainingTotal == 0:
+            return numberCoins
